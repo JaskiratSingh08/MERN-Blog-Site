47381149f2bb68239aa71ff1a66192da6c77469f
Jaskirat Singh <101934580+JaskiratSingh08@users.noreply.github.com>
2022-08-12 21:34:40 +0530

    Uploded backend files

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+const usersRoute = require('./routes/users');
+
+dotenv.config();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log(err));
+
+    app.use("/api/auth", authRoute);
+    app.use("/api/users", usersRoute);
+
+app.listen('4000',()=>{
+    console.log("App is lisening at the port")
+});
