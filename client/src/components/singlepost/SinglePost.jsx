6863a8487bfd7a11eaef0bb7ba29c8df16ddab29
Jaskirat Singh <101934580+JaskiratSingh08@users.noreply.github.com>
2022-08-22 21:56:43 +0530

    Added edit and delete mode

--- client/src/components/singlepost/SinglePost.jsx
+++ client/src/components/singlepost/SinglePost.jsx
@@ -11,11 +11,16 @@ function SinglePost() {
     const path = location.pathname.split('/')[2]
     const [post,setPost]= useState({});
     const {user} = useContext(Context) ;
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+    const [updateMode, setUpdateMode] = useState(false);
 
     useEffect(()=>{
         const getPost = async ()=>{
             const res = await axios.get('/posts/'+ path);
             setPost(res.data)
+            setTitle(res.data.title);
+            setDesc(res.data.desc);
         }
         getPost()
     },[path])
@@ -25,40 +30,72 @@ function SinglePost() {
             await axios.delete(`/posts/${post._id}`, 
             { data : {username:user.username} })
             window.location.replace("/")
+            //setUpdateMode(false)
             
         }catch(err){
             console.log(err)
         }
     }
 
+    const handleUpdate = async ()=>{
+        try{
+            await axios.put(`/posts/${post._id}`, 
+            {username:user.username, title, desc })
+            window.location.reload();
+            
+        }catch(err){
+            console.log(err)
+        }
+    }
   return (
     <div className="singlePost">
         <div className="singlePostWrapper">
             {post.photo &&  
-            
+                (
             <img className="singlePostImg"
             src={PF + post.photo}
             alt=''
             />
-            }
-            <h1 className="singlePostTitle">
-                {post.title}
+            )}
+            {updateMode ? (
+                <input 
+                type="text" 
+                value={title} 
+                className="singlePostTitleInput"
+                autoFocus
+                onChange={(e)=>setTitle(e.target.value)}/>
+            ) :
+            (
+
+                <h1 className="singlePostTitle">
+                {title}
                 {post.username === user?.username &&(
                     <div className="singlePostEdit">
-                        <i className="singlePostEditIcon fa-solid fa-pen"></i>
+                        <i className="singlePostEditIcon fa-solid fa-pen" onClick={()=>setUpdateMode(true)}></i>
                         <i className="singlePostEditIcon fa-solid fa-trash-can" onClick={handleDelete}></i>
                     </div>
                 )}
             </h1>
+                )}
             <div className="singlePostInfo">
                 <Link to={`/?user=${post.username}`} style={{textDecoration:"none",color:"inherit"}}>
                     <span className="singlePostAuthor">Author: <b> {post.username} </b></span>
                 </Link>
                 <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
             </div>
-            <p className="singlePostContent">
-                {post.desc}
+            {updateMode ? (
+                <textarea className="singlePostContentInput" value={desc} onChange={(e)=>setDesc(e.target.value)}/>
+            ) : (
+
+                <p className="singlePostContent">
+                {desc}
             </p>
+                )}
+                {updateMode &&
+                <button 
+                className="singlePostButton" 
+                onClick={handleUpdate}>UPDATE</button>
+            }
         </div>
     </div>
   )
